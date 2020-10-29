9c05d490a5044d86edbd4180e5a0ea1ae76191a4
Jiajie <jiajie.ma@thoughtworks.com>
2020-10-29 13:12:48 +0800

    [jiajie]add App.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Header from '../component/Header/Header';
+import GroupList from '../component/GroupList/GroupList';
+import TrainerList from '../component/TrainerList/TrainerList';
+import TraineeList from '../component/TraineeList/TraineeList';
 
 class App extends Component {
   render() {
     return (
       <div data-testid="app" className="App">
         <Header />
+        <GroupList />
+        <TrainerList />
+        <TraineeList />
       </div>
     );
   }
