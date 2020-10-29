61d76ca40f91dc2ac7daef37f5419fb7c7a7ec3c
Jiajie <jiajie.ma@thoughtworks.com>
2020-10-29 13:07:02 +0800

    [jiajie]add TrainerList component

--- src/component/TrainerList/TrainerList.js
+++ src/component/TrainerList/TrainerList.js
@@ -5,8 +5,8 @@ class TrainerList extends React.Component {
     super(props);
     this.state = {
       trainers: [
-        { id: 1, name: 'trainer1' },
-        { id: 2, name: 'trainer2' },
+        { id: 1, name: 'trainer1', grouped: false },
+        { id: 2, name: 'trainer2', grouped: false },
       ],
     };
   }
@@ -15,13 +15,15 @@ class TrainerList extends React.Component {
     return (
       <div>
         <span className="trainers-tittle">讲师列表</span>
-        {this.state.trainers.map((trainer) => (
-          <ul>
-            <li className="trainer-member">
-              ({trainer.id}.{trainer.name})
-            </li>
-          </ul>
-        ))}
+        {this.state.trainers
+          .filter((trainer) => trainer.grouped === false)
+          .map((trainer) => (
+            <ul>
+              <li className="trainer-member">
+                ({trainer.id}.{trainer.name})
+              </li>
+            </ul>
+          ))}
         <button type="button" className="add-trainer">
           +添加讲师
         </button>
